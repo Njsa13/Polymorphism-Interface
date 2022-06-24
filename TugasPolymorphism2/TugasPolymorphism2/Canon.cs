d50e043b576c5c987ba676e66e0093a6bffd8bf1
Najib Sauqi R <97015365+Njsa13@users.noreply.github.com>
2022-06-24 15:12:17 +0700

    Add files via upload

---
+++ TugasPolymorphism2/TugasPolymorphism2/Canon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism2
+{
+    public class Canon : IPrinterWindows
+    {
+        public void Show(PrinterWindows printer)
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+
+        public void Print(PrinterWindows printer)
+        {
+            Console.WriteLine("Canon printer printing....");
+        }
+    }
+}
---
+++ TugasPolymorphism2/TugasPolymorphism2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer = new PrinterWindows();
+
+            Console.WriteLine("Pilih Printer :");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet\n");
+
+            Console.Write("Nomor Printer [1...3] : ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            IPrinterWindows iprinter;
+            if (nomorPrinter == 1)
+            {
+                iprinter = new Epson();
+            }
+            else if (nomorPrinter == 2)
+            {
+                iprinter = new Canon();
+            }
+            else
+            {
+                iprinter = new LaserJet();
+            }
+
+            iprinter.Show(printer);
+            iprinter.Print(printer);
+
+            Console.ReadKey();
+        }
+    }
+}
